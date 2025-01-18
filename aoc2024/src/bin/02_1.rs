f84a9038c25656f876c43fca55aa975b94e940a7
Zasda Yusuf Mikail <zasdaym@gmail.com>
2025-01-18 22:57:55 +0700

    Add 2024 day 2

---
+++ aoc2024/src/bin/02_1.rs
@@ -0,0 +1,27 @@
+pub fn main() {
+    let result = include_str!("../../input/02.txt")
+        .lines()
+        .filter(|line| {
+            let report: Vec<i32> = line
+                .split_whitespace()
+                .filter_map(|x| x.parse().ok())
+                .collect();
+
+            if report.len() < 2 {
+                return false;
+            }
+
+            let is_increasing = report[1] > report[0];
+
+            report.windows(2).all(|pair| {
+                let diff = pair[1] - pair[0];
+
+                diff != 0
+                    && diff.abs() <= 3
+                    && ((is_increasing && diff > 0) || (!is_increasing && diff < 0))
+            })
+        })
+        .count();
+
+    println!("{}", result);
+}
---
+++ aoc2024/src/bin/02_2.rs
@@ -0,0 +1,56 @@
+pub fn main() {
+    let result = include_str!("../../input/02.txt")
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|x| x.parse().unwrap())
+                .collect::<Vec<i32>>()
+        })
+        .filter(|report| {
+            let size = report.len();
+            if is_safe_report(report, None) {
+                return true;
+            }
+            for i in 0..size {
+                if is_safe_report(report, Some(i)) {
+                    return true;
+                }
+            }
+            return false;
+        })
+        .count();
+
+    println!("{}", result);
+}
+
+fn is_safe_report(report: &[i32], skip: Option<usize>) -> bool {
+    let (mut back, mut front) = match skip {
+        Some(0) => (1, 2),
+        Some(1) => (0, 2),
+        _ => (0, 1),
+    };
+
+    let is_increasing = report[front] > report[back];
+
+    let skip = match skip {
+        Some(x) => x,
+        None => report.len() + 1,
+    };
+
+    while front < report.len() {
+        if front == skip {
+            front += 1;
+            continue;
+        }
+
+        let diff = report[front] - report[back];
+        if diff == 0 || diff.abs() > 3 || (is_increasing != (diff > 0)) {
+            return false;
+        }
+
+        back = front;
+        front += 1;
+    }
+
+    true
+}
