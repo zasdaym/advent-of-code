b0c6070df674fc63e572c48a40580ddce9e0bcc5
Zasda Yusuf Mikail <zasdaym@gmail.com>
2025-01-29 08:27:44 +0700

    Remove comments

--- aoc2024/src/bin/07_2.rs
+++ aoc2024/src/bin/07_2.rs
@@ -14,21 +14,18 @@ fn solve(input: &str) -> i128 {
                 .map(|c| c.parse::<i128>().unwrap())
                 .collect();
 
-            // Try all possible combinations of +, * and || operators
             let mut stack = vec![operands[0]];
             for &num in &operands[1..] {
                 let mut new_stack = Vec::new();
                 for &curr_sum in &stack {
                     new_stack.push(curr_sum + num);
                     new_stack.push(curr_sum * num);
-                    // Concatenate numbers by converting to string and back to number
                     let concatenated = format!("{}{}", curr_sum, num).parse::<i128>().unwrap();
                     new_stack.push(concatenated);
                 }
                 stack = new_stack;
             }
 
-            // Check if any combination equals target
             if stack.iter().any(|&result| result == target) {
                 target
             } else {
